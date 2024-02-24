de106bd52a07da4d0138cef4215511445ea01613
Vladimir Yablokov <YablokovWork@gmail.com>
2024-02-24 23:09:16 +0300

    начало поиска бага с сравнением дат.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {MyContext} from "./context";
 
 function App() {
     const [deals, setDeals] = useState([]);
-    console.log(deals)
 
     useEffect(() => {
         const deals = JSON.parse(localStorage.getItem('deals')) ?? [];
@@ -42,20 +41,10 @@ function App() {
     }
 
     const getFinishDate = (inputDate) => {
-        let startDate = convertDate(inputDate);
+        let startDate = new Date(inputDate);
         startDate.setDate(startDate.getDate() + 7);
-        return startDate
+        return convertDate(startDate.toISOString().split('T')[0])
     }
-
-    // const convertDate = (inputDate) => {
-    //     return new Date(inputDate);
-    // }
-
-    // const getFinishDate = (inputDate) => {
-    //     const startDate = convertDate(inputDate);
-    //     startDate.setDate(startDate.getDate() + 7);
-    //     return startDate;
-    // }
 
     return (
         <MyContext.Provider value={{deals, addDeal, closeDeal, clearDeals, convertDate, getFinishDate}}>
